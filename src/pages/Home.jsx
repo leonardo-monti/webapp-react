bfd4e1967eaf65ed00caad3eb4909d19077a05ca
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-17 15:44:37 +0100

    Milestone 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import axios from "axios"
+import { useState,useEffect } from "react"
+
 function Home(){
 
-    return <h1>Home</h1>
+const [movies,setMovies]=useState([])
+
+useEffect(()=>{
+    axios.get("http://localhost:3000/movies")
+    .then(res=>{
+        setMovies(res.data.movies)
+    })
+    .catch(err=>{
+        console.error(err)
+    })
+},[])
+
+return (
+<div>
+<h1>Movie List</h1>
+
+{movies.map((movie) => (
+  <div key={movie.id}>
+    <img
+      src={`http://localhost:3000/images/${movie.image}`}
+      alt={movie.title}
+    />
+    <h2>{movie.title}</h2>
+  </div>
+))}
+
+
+
+</div>
+)
 }
 
 export default Home
