19a369694a1a4ef4cb834a4e507570221637e83d
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-19 17:12:06 +0100

    Milestone 2 (frontend)

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function ReviewForm({movieId, addReview}) {
+
+const [formData,setFormData] =useState({
+    name:"",
+    vote:1,
+    text:"",
+
+})
+
+const handleFormChange=(e)=>{
+    const{name,value}=e.target
+
+    setFormData({...formData,[name]:value})
+}
+
+const handleFormSubmit=(e)=>{
+    e.preventDefault()
+
+    axios.post(`http://localhost:3000/movies/${movieId}/reviews`,formData)
+    .then(()=>{
+        addReview(formData)
+        setFormData({name:"",vote:1,text:""})
+    })
+    .catch((err)=>console.error(err))
+}
+
+return (
+    <div className="card mt-3">
+        <div className="card-header">
+            <h5>Add Review</h5>
+</div>
+<form className="card-body" onSubmit={handleFormSubmit}>
+    <div className="mb-2">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={formData.name}
+            onChange={handleFormChange}
+            required
+          />
+        </div>
+        <div className="mb-2">
+            <label className="form-label">Vote</label>
+            <select
+            className="form-select"
+            name="vote"
+            value={formData.vote}
+            onChange={handleFormChange}>
+                {[1, 2, 3, 4, 5].map((rank) => (
+              <option key={rank} value={rank}>
+                {rank}
+              </option>
+            ))}
+        </select>
+        </div>
+
+        <div className="mb-2">
+          <label className="form-label">Text</label>
+          <textarea
+            className="form-control"
+            name="text"
+            value={formData.text}
+            onChange={handleFormChange}
+            required>
+            </textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit Review
+        </button>
+     </form>
+    </div>
+)
+}
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -2,11 +2,14 @@ import axios from "axios"
 import { useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
 import Rating from "../components/Rating"
+import ReviewForm from "../components/ReviewForm"
+
 
 function MovieDetail(){
 
 const {id} = useParams()
 const [movie,setMovie]=useState(null)
+const [showForm, setShowForm]=useState(false)
 
 useEffect(()=>{
     axios.get(`http://localhost:3000/movies/${id}`)
@@ -16,35 +19,51 @@ useEffect(()=>{
 
 if(!movie) return <p>Loading...</p>
 
+const addReview= (newReview)=>{
+  setMovie({...movie,reviews:[...movie.reviews,{id: Date.now(), ...newReview}]
+})
+}
+
       return (
    <div className="container mt-4">
-  <div className="card mx-auto" style={{ maxWidth: "300px" }}>
-    <img
-      src={`http://localhost:3000/images/${movie.image}`}
-      className="card-img-top"
-      alt={movie.title}
-    />
-            <div className="card-body">
-              <h2 className="card-title">{movie.title}</h2>
-              <p><strong>Genre:</strong> {movie.genre}</p>
-              <p><strong>Director:</strong> {movie.director}</p>
-              <p><strong>Release year:</strong> {movie.release_year}</p>
-              <p><strong>Abstract:</strong> {movie.abstract}</p>
-            </div>
-          </div>
+      <div className="d-flex justify-content-between align-items-center mb-2">
+        <h2>{movie.title}</h2>
+        <button
+          className="btn btn-success"
+          onClick={() => setShowForm(!showForm)}
+        >
+          {showForm ? "Close Form" : "Add Review"}
+        </button>
+      </div>
 
-          <h4>Reviews</h4>
-          {movie.reviews.map((review) => (
-            <div key={review.id} className="card mb-2">
-              <div className="card-body">
-                <p><strong>{review.name}</strong> <Rating vote={review.vote} /></p>
-                <p>{review.text}</p>
-              </div>
-            </div>
-          ))}
+      {showForm && <ReviewForm movieId={movie.id} addReview={addReview} />}
 
+      <div className="card mx-auto mb-4 mt-4" style={{ maxWidth: "300px" }}>
+        <img
+          src={`http://localhost:3000/images/${movie.image}`}
+          className="card-img-top"
+          alt={movie.title}
+        />
+        <div className="card-body">
+          <p><strong>Genre:</strong> {movie.genre}</p>
+          <p><strong>Director:</strong> {movie.director}</p>
+          <p><strong>Release year:</strong> {movie.release_year}</p>
+          <p><strong>Abstract:</strong> {movie.abstract}</p>
         </div>
-    
+      </div>
+
+      <h4>Reviews</h4>
+      {movie.reviews.map((review) => (
+        <div key={review.id} className="card mb-2">
+          <div className="card-body">
+            <p>
+              <strong>{review.name}</strong> <Rating vote={review.vote} />
+            </p>
+            <p>{review.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
